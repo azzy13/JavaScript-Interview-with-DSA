64711188f0e3be95d6ef0980067581e76845006c
Dhananjay Kumar <dhananjaysingh11060@gmail.com>
2023-02-12 11:16:16 +0530

    Add Roman to integer code

--- leetcode/set-1.js
+++ leetcode/set-1.js
@@ -45,7 +45,7 @@
  */
 
 var twoSum = function (nums, target) {
-     const hash = {};
+     let hash = {};
 
      for (let i = 0; i < nums.length; i++) {
           let value = nums[i];
@@ -103,4 +103,29 @@ var addTwoNumbers = function (l1, l2) {};
  * @return {number}
  */
 
-var romanToInt = function (s) {};
+var romanToInt = function (s) {
+     let result = 0;
+     let hash = {
+          I: 1,
+          V: 5,
+          X: 10,
+          L: 50,
+          C: 100,
+          D: 500,
+          M: 1000,
+     };
+
+     for (let i = 0; i < s.length; i++) {
+          let curr = hash[s[i]];
+          let next = hash[s[i + 1]];
+          //IV
+          if (curr < next) {
+               result += next - curr;
+               i++;
+          } else {
+               result += curr;
+          }
+     }
+     return result;
+};
+console.log(romanToInt('IV')); //4
